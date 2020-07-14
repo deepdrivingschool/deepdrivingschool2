ff24a18f2267a29d4317cd39b7fafac2bc021eb8
deepdrivingschool <68257535+deepdrivingschool@users.noreply.github.com>
2020-07-13 18:36:42 -0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<footer class="container-fluid text-center" id="footer">
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <h2>Deep Driving School</h2>
+  <p><span class="facebook"><a href="#"><img src="includes/images/facebook.png"></a></span><span class="twitter"><a href="#"><img src="includes/images/twitter.png"></a></span></p>
+  <h2><span class="glyphicon glyphicon-phone"></span> +1 778.887.5600
+  <br/>
+  <span class="glyphicon glyphicon-phone"></span> +1 604.600.1632</h2>
+  <p>© Copyright 2016: DeepDrivingSchoolLTD, All rights are reserved</p>
+</footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Deep Driving School Surrey BC | Driving Lessons Delta | Driver Training Course</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="includes/css/style.css" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="/">Deep Driving School</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="home">HOME</a></li>
+        <li><a href="services">SERVICES</a></li>
+        <li><a href="contact-us">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
